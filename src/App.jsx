9d8b28d06a352a615fa8ab9a96c5e478e3f4bdec
moralessf <fcesar_moan@hotmail.com>
2022-05-17 19:55:29 -0500

    Correction of App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 import './App.styles.scss';
-import Login from './Components/Login/Login';
 
 function App() {
   return (
