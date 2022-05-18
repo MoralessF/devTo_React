34d4fb9489389c3e185c5e8a9963a3b657fbafd2
Martha Luna <lunam91@hotmail.com>
2022-05-18 13:53:07 -0500

    fixing navbar

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,14 +3,14 @@ import './Main.scss';
 import AsideLeft from '../AsideLeft';
 import AsideRight from '../AsideRight';
 import AsideCenter from '../AsideCenter';
-import NavBar from '../NavBar';
+
 
 const Main = () => {
   return (
     <div className="Main">
       <div className="Main-Container">
         <div className="Main-Content">
-        <div><NavBar/></div>
+       
         <div className='Home'>
         <AsideLeft />
           <AsideCenter/>
