bb891cc5716db269b3ff84e6b86055b782f6fc4e
moralessf <fcesar_moan@hotmail.com>
2022-05-17 23:04:16 -0500

    Login section with styles. Pending JS code implementation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import Login from './Components/Login/Login';
 import './App.css';
 import './App.styles.scss';
@@ -7,10 +6,7 @@ import './App.styles.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-        <Button className="btn-danger">Hola</Button>
-      </header>
+      <Login />
     </div>
   );
 }
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,83 +1,119 @@
 import React from 'react';
 import './Login.styles.scss';
+import { BsApple, BsGithub, BsTwitter, BsEmojiWink } from 'react-icons/bs';
+
+const loginMethods = [
+  { name: 'Apple', icon: <BsApple className="buttonIcon" /> },
+  { name: 'Forem', icon: <BsEmojiWink className="buttonIcon" /> },
+  { name: 'GitHub', icon: <BsGithub className="buttonIcon" /> },
+  { name: 'Twitter', icon: <BsTwitter className="buttonIcon" /> },
+];
+
+const loginInput = [
+  {
+    textLabel: 'Email',
+    idInput: 'emailInput',
+    typeInput: 'email',
+    nameInput: 'email',
+  },
+  {
+    textLabel: 'Password',
+    idInput: 'passwordInput',
+    typeInput: 'password',
+    nameInput: 'password',
+  },
+];
 
 const Login = () => {
   return (
-    <main>
-      <div>
-        <div>
-          <h1>Welcome to DEV Community</h1>
-          <p>
-            <a>DEV Community</a> is a community of 851,532 amazing developers
-          </p>
-        </div>
-        <div>
-          <div classNameName="d-flex flex-column">
-            <button type="button" className="mb-2 p-2">
-              <i className="me-1 bi bi-apple" />
-              Continue with Apple
-            </button>
-            <button type="button" className="mb-2 p-2">
-              <i className="me-1 bi bi-github" />
-              Continue with Forem
-            </button>
-            <button type="button" className="mb-2 p-2">
-              <i className="me-1 bi bi-emoji-laughing" />
-              Continue with GitHub
-            </button>
-            <button type="button" className="mb-2 p-2">
-              <i className="me-1 bi bi-twitter" />
-              Continue with Twitter
-            </button>
+    <div className="body">
+      <main className="container">
+        <div className="container bg-white mt-2 m-md-4 p-md-4">
+          <div className="d-flex flex-column">
+            <h1 className="text-center loginTitle">Welcome to DEV Community</h1>
+            <p className="text-center">
+              <a className="loginAnchors" href="">
+                DEV Community
+              </a>{' '}
+              is a community of 851,532 amazing developers
+            </p>
           </div>
-          <div>
-            <div className="text-center mt-2 mb-1">
-              <span className="">
-                Have a password? Continue with your email address
-              </span>
+          <div className="p-3">
+            <div className="d-flex flex-column">
+              {loginMethods.map((button) => {
+                return (
+                  <button
+                    className={`${button.name} loginButton p-2 mb-2 d-flex align-items-center justify-content-center`}
+                    key={button.name}
+                  >
+                    {button.icon}
+                    Continue with {button.name}
+                  </button>
+                );
+              })}
             </div>
-            <form onSubmit="logIn(event)">
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                  Email
-                  <input
-                    id="emailInput"
-                    type="text"
-                    name="email"
-                    className="form-control"
-                  />
-                </label>
-
-                <label htmlFor="password" className="form-label">
-                  Password
-                  <input
-                    id="passwordInput"
-                    type="password"
-                    name="password"
-                    className="form-control"
-                  />
-                </label>
+            <div>
+              <div className="text-center mt-2 mb-1">
+                <span className="">
+                  Have a password? Continue with your email address
+                </span>
               </div>
+              <form>
+                <div className="mb-3">
+                  {loginInput.map((group) => {
+                    return (
+                      <>
+                        <label className="form-label mt-2">
+                          {group.textLabel}
+                        </label>
+                        <input
+                          className="form-control mb-3"
+                          id={group.idInput}
+                          type={group.typeInput}
+                          name={group.nameInput}
+                        ></input>
+                      </>
+                    );
+                  })}
+                  {/* <label className="form-label">Email</label>
+                <input
+                  id="emailInput"
+                  type="email"
+                  name="email"
+                  className="form-control"
+                />
 
-              <div className="mb-3">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" />
-                  <p className="form-check-label">Remember me</p>
+                <label className="form-label">Password</label>
+                <input
+                  id="passwordInput"
+                  type="password"
+                  name="password"
+                  className="form-control"
+                /> */}
                 </div>
-              </div>
 
-              <button type="submit" className="btn btn-primary w-100">
-                Continue
-              </button>
+                <div className="mb-3">
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" />
+                    <label className="form-check-label">Remember me</label>
+                  </div>
+                </div>
 
-              <p className="text-center m-4">
-                <p>I forgot my password</p>
-              </p>
-            </form>
+                <button type="submit" className="btn btn-primary w-100">
+                  Continue
+                </button>
+
+                <p className="text-center m-4">
+                  <a className="loginAnchors supportAnchor" href="">
+                    I forgot my password
+                  </a>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 };
 
